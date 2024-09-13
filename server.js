766a658afc442ed564520d781e7f0a6a8cd9f4a1
themerous <lhs981130@gmail.com>
2024-09-13 19:41:52 +0900

    testing

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.static('public')); // Serve static files
 
-app.get('/nws/health', (req, res) => {
+app.get('/health', (req, res) => {
   res.status(200).send('Server is running');
 });
 
