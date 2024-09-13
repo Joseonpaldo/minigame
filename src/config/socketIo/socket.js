8df86ec7942e68c9cb0404efec5b549d68ed74bd
themerous <lhs981130@gmail.com>
2024-09-13 19:29:29 +0900

    testing

--- src/config/socketIo/socket.js
+++ src/config/socketIo/socket.js
@@ -11,7 +11,7 @@ exports.initSocket = (server) => {
     const io = socketIo(server, {
         path: '/nws',
         cors: {
-            origin: "*",
+            origin: "https://joseonpaldo.site",
             methods: ['GET', 'POST']
         }
     });
